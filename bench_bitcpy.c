56fd578e47091c3f7acde45c828689534792ca8e
YCYao <BlackYao@dmp.com.tw>
2021-03-10 12:01:51 +0800

    Fix clang-format

--- bench_bitcpy.c
+++ bench_bitcpy.c
@@ -1,6 +1,6 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <time.h>
 #include "bitcpy.h"
 
@@ -8,15 +8,16 @@ static uint8_t input[4096], output[4096];
 
 static inline long long elapsed(struct timespec t1, struct timespec t2)
 {
-    return (long long)(t2.tv_sec - t1.tv_sec) * 1e9 + (long long)(t2.tv_nsec - t1.tv_nsec);
+    return (long long) (t2.tv_sec - t1.tv_sec) * 1e9 +
+           (long long) (t2.tv_nsec - t1.tv_nsec);
 }
 
-int main(int argc, char**argv)
+int main(int argc, char **argv)
 {
     struct timespec t1, t2;
     memset(&input[0], 0xFF, sizeof(input));
-    
-    for(int i = 1; i < 8000; i++){
+
+    for (int i = 1; i < 8000; i++) {
         size_t r, w;
         r = rand() % 8;
         w = rand() % 8;
@@ -37,8 +38,7 @@ int main(int argc, char**argv)
         bitcpy_branch_predict(&output[0], w, &input[0], r, i);
         clock_gettime(CLOCK_REALTIME, &t2);
         printf("%lld\n", elapsed(t1, t2));
-
     }
-    
+
     return 0;
 }
