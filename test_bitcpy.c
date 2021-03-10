9b0713d0c8eb7183699f4e51f037350b864cdb62
YCYao <BlackYao@dmp.com.tw>
2021-03-10 11:58:34 +0800

    Add bitcpy validation

--- test_bitcpy.c
+++ test_bitcpy.c
@@ -1,8 +1,9 @@
+#include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
 #include "bitcpy.h"
 
-static uint8_t output[8], input[8];
+static uint8_t output[8], input[8], output_align[8];
 
 static inline void dump_8bits(uint8_t _data)
 {
@@ -17,6 +18,13 @@ static inline void dump_binary(uint8_t *_buffer, size_t _length)
         dump_8bits(*_buffer++);
 }
 
+static void validate(uint8_t *r1, uint8_t *r2, size_t size)
+{
+    for (size_t i = 0; i < size; i++) {
+        assert(*r1++ == *r2++);
+    }
+}
+
 int main(int _argc, char **_argv)
 {
     memset(&input[0], 0xFF, sizeof(input));
@@ -24,11 +32,19 @@ int main(int _argc, char **_argv)
     for (int i = 1; i <= 32; ++i) {
         for (int j = 0; j < 16; ++j) {
             for (int k = 0; k < 16; ++k) {
-                memset(&output[0], 0x00, sizeof(output));
                 printf("%2d:%2d:%2d ", i, k, j);
-                bitcpy_align(&output[0], k, &input[0], j, i);
+                memset(&output[0], 0x00, sizeof(output));
+                bitcpy(&output[0], k, &input[0], j, i);
                 dump_binary(&output[0], 8);
                 printf("\n");
+
+                printf("%2d:%2d:%2d ", i, k, j);
+                memset(&output_align[0], 0x00, sizeof(output));
+                bitcpy_align(&output_align[0], k, &input[0], j, i);
+                dump_binary(&output_align[0], 8);
+                printf("\n");
+
+                validate(output, output_align, 8);
             }
         }
     }
